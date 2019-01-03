01ec4ba7081a32395065b54e4679e090103d7d6c
cowglow <cowglow@gmail.com>
2019-01-03 18:57:28 +0100

    Working Progress: Disclaimer Banner Directive Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DisclaimerBannerDirective } from './disclaimer-banner.directive';
 
+
 @NgModule({
   declarations: [
     AppComponent,
